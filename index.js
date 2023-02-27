de06aa23fddde663f2d70beb00c88b6cf7aec23c
DreamMln <maix2374@edu.zealand.dk>
2023-02-27 09:39:55 +0100

    VUEOpretBil

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-const baseUrl = "https://myressourcem.azurewebsites.net/api/carshare"
+//const baseUrl = "https://myressourcem.azurewebsites.net/api/carshare"
+const carRidesUrl = "https://localhost:44343/Api/CarRides"
+const carUrl = "https://localhost:44343/Api/Cars"
+const accountUrl = "https://localhost:44343/Api/Accounts"
 
 Vue.createApp({
     data() {
         return {
             carArray: [],
             error: null,
+            
+            accountArray: [],
+            getAccountData: "",
+            getMessage: "",
+            accountId: 0,
+
+            cars: [],
             carId: "",
-            postCars: { 
-                carId: 0,
-                accountId: 0,
-                
-            },
+            addData: { brand: "", model:"", fuelType:"" },
+            addMessage: "",
 
         }
     },
     created() { // life cycle method. Called when browser reloads page
+        this.getAllRides()
+        this.getAllAccounts()
         this.getAllCars()
     },
     methods: {
-        async getAllCarsHelper(url){
+       //istedet for at genskrive koden, gør jeg den mere dry ved at lave en helper
+        async getAllCarRidesHelper(url){
             try {//fejl håndtering 
                 const result = await axios.get(url)
                 this.carArray= result.data
@@ -28,24 +38,63 @@ Vue.createApp({
                 alert(ex.message) 
             }
         },
-        getAllCars(){
-            console.log("Getting the get method")
-            this.getAllCarsHelper(baseUrl)
-            //istedet for at genskrive koden, gør jeg den mere dry ved at lave en helper
-        },
-        
-        // cleanList() {
-        //     this.carArray = []
-        //     this.error = null
-        // },
-
-        postCars(){
-            try{
-                const result = axios.post(baseUrl)
-            }catch(ex){
-
+        async getAllAccountsHelper(url){
+            try {//fejl håndtering 
+                const result = await axios.get(url)
+                this.accountArray= result.data
+                console.log(this.carArray)
+                //console.writeline udskriver til konsollen
+            } catch (ex) {//exception
+                alert(ex.message) 
             }
-        }
+        },
+        async getAllCarsHelper(url){
+            try {//fejl håndtering 
+                const result = await axios.get(url)
+                this.cars= result.data
+                console.log(this.cars)
+                //console.writeline udskriver nogle
+            } catch (ex) {//exception
+                alert(ex.message) 
+            }
+        },
+        //GET ALL METODER
+        getAllRides(){
+            console.log("Getting the car ride get method")
+            this.getAllCarRidesHelper(carRidesUrl)
+        },
+        getAllAccounts(){
+            console.log("Getting the account get method")
+            this.getAllAccountsHelper(accountUrl)
+        },
+        getAllCars(){
+            console.log("Getting the car ride get method")
+            this.getAllCarsHelper(carUrl)
+        },
+
+        //GET BY ID METODER
+        async getAccountsId(){
+            const url = baseUrl + this.accountId
+            try{
+                const response = await axios.get(url)
+                this.getAccountData = response.data //her bliver getAccountData fyldt ud med data
+                this.getMessage = response.status + " " + response.statusText
+                console.log("Get the FK accountId")
+            }catch(ex){
+                alert(ex.message) 
+            }
+        },
+
+        //POST METODER
+        async postCar(){
+            try{
+                response= await axios.post(carUrl, this.addData)
+                this.addMessage="Response: " + response.status + " " + response.statusText
+                this.getAllCars()
+            }catch(ex){
+                alert(ex.message)
+            }
+        },
 
     }
 }).mount("#app")
