6f10291d070aed487241ff2260b91a1085a896f4
DreamMln <maix2374@edu.zealand.dk>
2023-02-28 14:15:14 +0100

    OpretKontoKanOprette

---
+++ createAccount.js
@@ -0,0 +1,61 @@
+const baseUrl = "https://mitlift.azurewebsites.net/Api"
+
+const createAccount = Vue.createApp({
+    data() {
+        return {
+            accountArray: [],
+            getAccountData: "",
+            getMessage: "",
+            addMessage: "",
+            accountId: 0,
+
+            addData: {
+                accountId: 0,
+                userName: "",
+                dateOfBirth: "",
+                userAddress: "",
+                phone:"",
+                email:""
+              },
+        }
+    },
+    created() { // life cycle method. Called when browser reloads page
+        this.getAllAccounts()
+    },
+    methods: {
+       //istedet for at genskrive koden, gør jeg den mere dry ved at lave en helper
+        async getAllAccountsHelper(url){
+            try {//fejl håndtering 
+                const result = await axios.get(url)
+                this.accountArray= result.data
+                console.log(this.accountArray)
+                //console.writeline udskriver til konsollen
+            } catch (ex) {//exception
+                alert(ex.message) 
+            }
+        },
+       
+        //GET ALL METODER
+        getAllAccounts(){
+            console.log("Getting the account get method")
+            this.getAllAccountsHelper(baseUrl + "/Accounts")
+        },
+        //POST METODER
+        async postAccount(){
+            try{
+                response = await axios.post(baseUrl + "/Accounts", this.addData)
+                this.addMessage="Response: " + response.status + " " + response.statusText
+                if (response.status == 200) {
+                    location.href="/index.html"
+                }
+                else{
+                    alert("Der er noget galt her " + response.status)
+                }
+                
+            }catch(ex){
+                alert(ex.message)
+            }
+        },
+    }
+})
+createAccount.mount("#createAccount")
